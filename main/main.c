3e087c55cd4bcf476f3c9af99151cd2f335fe1ee
Henry Zheng <zhengruiw02@gmail.com>
2024-03-24 18:06:54 +0800

    Create new branch as ssh terminal project. Use console_wifi instead of app config for wifi sta mode connection.

--- main/main.c
+++ main/main.c
@@ -28,44 +28,22 @@
 #include "lwip/err.h"
 #include "lwip/sys.h"
 
+// for console_wifi & console_ping
+#include "esp_netif.h"
+#include "nvs_flash.h"
+#include "esp_event.h"
+// for console_wifi
+#include "console_wifi.h"
+// for console_ping
+#include "console_ping.h"
 
-/* FreeRTOS event group to signal when we are connected*/
-static EventGroupHandle_t s_wifi_event_group;
-
-/* The event group allows multiple bits for each event, but we only care about two events:
- * - we are connected to the AP with an IP
- * - we failed to connect after the maximum amount of retries */
-#define WIFI_CONNECTED_BIT BIT0
-#define WIFI_FAIL_BIT	   BIT1
 
 static const char *TAG = "MAIN";
 
-static int s_retry_num = 0;
 
 EventGroupHandle_t xEventGroup;
 int TASK_FINISH_BIT	= BIT4;
 
-static void event_handler(void* arg, esp_event_base_t event_base,
-								int32_t event_id, void* event_data)
-{
-	if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
-		esp_wifi_connect();
-	} else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
-		if (s_retry_num < CONFIG_ESP_MAXIMUM_RETRY) {
-			esp_wifi_connect();
-			s_retry_num++;
-			ESP_LOGI(TAG, "retry to connect to the AP");
-		} else {
-			xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
-		}
-		ESP_LOGI(TAG,"connect to the AP fail");
-	} else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
-		ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
-		ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
-		s_retry_num = 0;
-		xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
-	}
-}
 
 static void SPIFFS_Directory(char * path) {
 	DIR* dir = opendir(path);
@@ -78,92 +56,41 @@ static void SPIFFS_Directory(char * path) {
 	closedir(dir);
 }
 
-void wifi_init_sta(void)
+static void netif_nvs_flash_init(void)
 {
-	s_wifi_event_group = xEventGroupCreate();
-
-	ESP_ERROR_CHECK(esp_netif_init());
-
-	ESP_ERROR_CHECK(esp_event_loop_create_default());
-	esp_netif_create_default_wifi_sta();
-
-	wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-	ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-
-	esp_event_handler_instance_t instance_any_id;
-	esp_event_handler_instance_t instance_got_ip;
-	ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
-					ESP_EVENT_ANY_ID,
-					&event_handler,
-					NULL,
-					&instance_any_id));
-	ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
-					IP_EVENT_STA_GOT_IP,
-					&event_handler,
-					NULL,
-					&instance_got_ip));
-
-	wifi_config_t wifi_config = {
-		.sta = {
-			.ssid = CONFIG_ESP_WIFI_SSID,
-			.password = CONFIG_ESP_WIFI_PASSWORD,
-			/* Setting a password implies station will connect to all security modes including WEP/WPA.
-			 * However these modes are deprecated and not advisable to be used. Incase your Access point
-			 * doesn't support WPA2, these mode can be enabled by commenting below line */
-		 .threshold.authmode = WIFI_AUTH_WPA2_PSK,
-
-			.pmf_cfg = {
-				.capable = true,
-				.required = false
-			},
-		},
-	};
-	ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
-	ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
-	ESP_ERROR_CHECK(esp_wifi_start() );
-
-	ESP_LOGI(TAG, "wifi_init_sta finished.");
-
-	/* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
-	 * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
-	EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
-			WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
-			pdFALSE,
-			pdFALSE,
-			portMAX_DELAY);
-
-	/* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
-	 * happened. */
-	if (bits & WIFI_CONNECTED_BIT) {
-		ESP_LOGI(TAG, "connected to ap SSID:%s",
-				 CONFIG_ESP_WIFI_SSID);
-	} else if (bits & WIFI_FAIL_BIT) {
-		ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s",
-				 CONFIG_ESP_WIFI_SSID, CONFIG_ESP_WIFI_PASSWORD);
-	} else {
-		ESP_LOGE(TAG, "UNEXPECTED EVENT");
-	}
-
-	/* The event will not be processed after unregister */
-	ESP_ERROR_CHECK(esp_event_handler_instance_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, instance_got_ip));
-	ESP_ERROR_CHECK(esp_event_handler_instance_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, instance_any_id));
-	vEventGroupDelete(s_wifi_event_group);
+  ESP_ERROR_CHECK(esp_netif_init());
+  ESP_ERROR_CHECK(esp_event_loop_create_default());
+  esp_err_t ret = nvs_flash_init();   //Initialize NVS
+  if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+  }
+  ESP_ERROR_CHECK(ret);
 }
+
+static void console_init(void)
+{
+  ESP_ERROR_CHECK(console_cmd_init());     // Initialize console
+  // Register all plugin command added to your project
+  ESP_ERROR_CHECK(console_cmd_all_register());
+  // To register only wifi command skip calling console_cmd_all_register()
+  ESP_ERROR_CHECK(console_cmd_wifi_register());  
+  // To register only ifconfig command skip calling console_cmd_all_register()
+  ESP_ERROR_CHECK(console_cmd_ping_register());
+  ESP_ERROR_CHECK(console_cmd_start());    // Start console
+}
+
 
 void ssh_task(void *pvParameters);
 
 void app_main(void)
 {
-	//Initialize NVS
-	esp_err_t ret = nvs_flash_init();
-	if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-	  ESP_ERROR_CHECK(nvs_flash_erase());
-	  ret = nvs_flash_init();
-	}
-	ESP_ERROR_CHECK(ret);
+	esp_err_t ret;
+	//Initialize NVS	
+	netif_nvs_flash_init();
 
-	ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
-	wifi_init_sta();
+	//Initialize Wi-Fi & ping console
+	console_init();
 
 	ESP_LOGI(TAG, "Initializing SPIFFS");
 	esp_vfs_spiffs_conf_t conf = {
