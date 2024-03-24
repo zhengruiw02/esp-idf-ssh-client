386d8e0104b3f17bfc737e98d078965bec10dc24
Henry Zheng <zhengruiw02@gmail.com>
2024-03-25 01:00:28 +0800

    Add consloe_ssh submodule, which move ssh code to there.

--- main/main.c
+++ main/main.c
@@ -37,24 +37,9 @@
 // for console_ping
 #include "console_ping.h"
 
+#include "console_ssh.h"
 
 static const char *TAG = "MAIN";
-
-
-EventGroupHandle_t xEventGroup;
-int TASK_FINISH_BIT	= BIT4;
-
-
-static void SPIFFS_Directory(char * path) {
-	DIR* dir = opendir(path);
-	assert(dir != NULL);
-	while (true) {
-		struct dirent*pe = readdir(dir);
-		if (!pe) break;
-		ESP_LOGI(__FUNCTION__,"d_name=%s d_ino=%d d_type=%x", pe->d_name,pe->d_ino, pe->d_type);
-	}
-	closedir(dir);
-}
 
 static void netif_nvs_flash_init(void)
 {
@@ -77,6 +62,8 @@ static void console_init(void)
   ESP_ERROR_CHECK(console_cmd_wifi_register());  
   // To register only ifconfig command skip calling console_cmd_all_register()
   ESP_ERROR_CHECK(console_cmd_ping_register());
+  ESP_ERROR_CHECK(console_cmd_ssh_register());
+  
   ESP_ERROR_CHECK(console_cmd_start());    // Start console
 }
 
@@ -91,63 +78,5 @@ void app_main(void)
 
 	//Initialize Wi-Fi & ping console
 	console_init();
-
-	ESP_LOGI(TAG, "Initializing SPIFFS");
-	esp_vfs_spiffs_conf_t conf = {
-		.base_path = "/spiffs",
-		.partition_label = NULL,
-		.max_files = 8,
-		.format_if_mount_failed = true
-	};
-
-	// Use settings defined above toinitialize and mount SPIFFS filesystem.
-	// Note: esp_vfs_spiffs_register is anall-in-one convenience function.
-	ret = esp_vfs_spiffs_register(&conf);
-
-	if (ret != ESP_OK) {
-		if (ret == ESP_FAIL) {
-			ESP_LOGE(TAG, "Failed to mount or format filesystem");
-		} else if (ret == ESP_ERR_NOT_FOUND) {
-			ESP_LOGE(TAG, "Failed to find SPIFFS partition");
-		} else {
-			ESP_LOGE(TAG, "Failed to initialize SPIFFS (%s)",esp_err_to_name(ret));
-		}
-		return;
-	}
-	SPIFFS_Directory("/spiffs/");
-
-	// Create Eventgroup
-	xEventGroup = xEventGroupCreate();
-	configASSERT( xEventGroup );
-
-	ESP_LOGI(TAG, "Opening file");
-	FILE* fp = fopen("/spiffs/command.txt", "r");
-	if (fp == NULL) {
-		ESP_LOGE(TAG, "Failed to open file for reading");
-		return;
-	}
-
-	char line[256];
-	while(fgets(line, sizeof(line), fp) != NULL) {
-		int lineLen = strlen(line);
-		line[lineLen-1] = 0;
-		ESP_LOGI(TAG, "line=[%s] lineLen=%d", line, lineLen);
-		if (lineLen == 1) continue;
-		if (line[0] == '#') continue;
-
-		// Execute ssh command
-		xEventGroupClearBits( xEventGroup, TASK_FINISH_BIT );
-		xTaskCreate(&ssh_task, "SSH", 1024*8, (void *) &line, 2, NULL);
-
-		// Wit for ssh finish.
-		xEventGroupWaitBits( xEventGroup,
-			TASK_FINISH_BIT,	/* The bits within the event group to wait for. */
-			pdTRUE,				/* HTTP_CLOSE_BIT should be cleared before returning. */
-			pdFALSE,			/* Don't wait for both bits, either bit will do. */
-			portMAX_DELAY);		/* Wait forever. */
-	}
-	fclose(fp);
-
-	ESP_LOGI(TAG, "SSH all finish");
 
 }
